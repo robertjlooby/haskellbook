80de1a819c430ed320f75e3537a96cea2c38f5f4
Rob Looby <robertjlooby@gmail.com>
2018-05-27 21:18:15 -0500

    Do Chapter 7 exercises

--- spec.hs
+++ spec.hs
@@ -1,6 +1,9 @@
-import Test.Hspec
 import Chapter06
+import Chapter07
+import Test.Hspec
 
 main :: IO ()
-main = hspec $ do
-  Chapter06.spec
+main =
+  hspec $ do
+    Chapter06.spec
+    Chapter07.spec
